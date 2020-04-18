5f83e5a9bf3b0497bb420628762aa294413d58b7
Mikhail Kuklin <mihan007@ya.ru>
2020-04-18 21:33:17 +0300

    Add form to enter address and contact at order page
    Close #14, close #18

--- src/components/Order.jsx
+++ src/components/Order.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Button, Grid } from 'semantic-ui-react'
+import { Button, Form, Grid } from 'semantic-ui-react'
 import OrderLine from '../containers/OrderLine'
 import _ from 'lodash'
 
@@ -8,7 +8,7 @@ export default class OrderComponent extends Component {
     return (
       <Grid centered columns={1} className={'order'}>
         <Grid.Column width={8}>
-          <h1>Your order</h1>
+          <h2>Your order</h2>
           {_.sortBy(this.props.items, function (o) {return o.title}).map(product => (<OrderLine {...product} />))}
           {this.props.count === 0 ?
             <h4><i>Nothing. Maybe you aren't hungry yet?</i></h4>
@@ -19,9 +19,20 @@ export default class OrderComponent extends Component {
               </span>,
               <p className={'totalLine'}>Total price:
                 €{(this.props.totalEurPrice + this.props.deliveryEurPrice) / 100},
-                ${(this.props.totalUsdPrice + this.props.deliveryUsdPrice)/100}</p>,
-              <h2>Delivery Address</h2>,
-              <h2>Contact information</h2>,
+                ${(this.props.totalUsdPrice + this.props.deliveryUsdPrice) / 100}</p>,
+              <h2>Delivery and contact information</h2>,
+              <Form>
+                <Form.Group>
+                  <Form.Input label='Address' placeholder='Address' width={16}/>
+                </Form.Group>
+                <Form.Group>
+                  <Form.Input label='Name' placeholder='Name' width={6}/>
+                  <Form.Input label='Email' placeholder='Email' width={6}/>
+                  <Form.Input label='Phone' placeholder='Phone' width={4}/>
+                </Form.Group>
+                <Form.Checkbox label='Please call me to confirm the order'/>
+              </Form>,
+              <br/>,
               <Button primary fluid>Place the order</Button>,
               <i>Work in progress...</i>
             ]
